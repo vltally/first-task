5c48d519f0ca1f543250ee6125f2ad8d5cefbb63
vltally <vitalik.koliesnikov@gmail.com>
2023-01-27 23:17:57 +0200

    adapt were done

--- js/script.js
+++ js/script.js
@@ -9,3 +9,33 @@ if (iconMenu) {
         menuBody.classList.toggle('_active');
     });
 }
+
+//scroll
+
+const menuLinks = document.querySelectorAll('.nav-element[data-goto]');
+
+console.log(menuLinks);
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+}
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY - document.querySelector('.navbar-mobile').offsetHeight;
+
+            if(iconMenu.classList.contains("_active")){
+                document.body.classList.remove("_lock");
+                iconMenu.classList.remove("_active");
+                menuBody.classList.remove("_active");
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
