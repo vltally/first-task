61297bc61aad5abe415bea6b839fa3c8f7b4e6f5
vltally <vitalik.koliesnikov@gmail.com>
2023-02-01 21:31:06 +0200

    lazy load were addded

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ if (iconMenu) {
 
 const menuLinks = document.querySelectorAll('.nav-element[data-goto]');
 
-console.log(menuLinks);
+
 if (menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
         menuLink.addEventListener("click", onMenuLinkClick);
@@ -23,8 +23,6 @@ if (menuLinks.length > 0) {
 }
     function onMenuLinkClick(e) {
         const menuLink = e.target;
-        console.log(menuLink.dataset.goto);
-        console.log(document.querySelector(menuLink.dataset.goto));
         if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY - document.querySelector('.nav').offsetHeight;
@@ -43,3 +41,45 @@ if (menuLinks.length > 0) {
             e.preventDefault();
         }
     }
+
+
+/* lazy load*/
+
+const lazyImages = document.querySelectorAll("img[data-src]");
+
+const windowHeight = document.documentElement.clientHeight;
+
+
+let lazyImagesPositions = [];
+
+if(lazyImages.length > 0){
+    lazyImages.forEach(img =>{
+        if(img.dataset.src){
+            lazyImagesPositions.push(img.getBoundingClientRect().top + window.scrollY)
+            
+            lazyScrollCheck();
+        }
+    });
+}
+
+window.addEventListener("scroll", lazyScroll);
+
+function lazyScroll(){
+    if(document.querySelectorAll("img[data-src]").length > 0){
+        lazyScrollCheck()
+    }
+}
+
+function lazyScrollCheck(){
+    let imgIndex = lazyImagesPositions.findIndex(
+        item => window.scrollY > item - windowHeight
+    );
+
+    if(imgIndex >= 0){
+        if(lazyImages[imgIndex].dataset.src){
+            lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+            lazyImages[imgIndex].removeAttribute('data-src');
+        } 
+        delete lazyImagesPositions[imgIndex];
+    }
+}
